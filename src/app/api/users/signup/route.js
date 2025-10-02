51f845e0a3891a61783e7379f4c40dac5418480e
do4best657 <panic_4_what@hotmail.com>
2025-10-02 17:12:15 +0500

    email sending work is underway

--- src/app/api/users/signup/route.js
+++ src/app/api/users/signup/route.js
@@ -1,3 +1,29 @@
 import {dbConfig} from "@/dbConfig/dbConfig";
-import User from "@/models/user.model"
+import User from "@/models/user.model";
+import bscryptjs from "bcryptjs";
 import {NextResponse,NextRequest} from "next/server";
+import {sendEmail} from "@/helpers/mailer";
+dbConfig()
+export async function POST(req,res){
+try{
+    const requestBody=await req.json()
+    const {username,email,password}=requestBody;
+    const user=await User.findOne({email})
+    if(user){
+        return NextResponse.json({message:"User already exists"},{status:400})
+    }
+
+    const salt=await bscryptjs.genSalt(10);
+    const hashPassword=await bscryptjs.hash(password,salt);
+new User({
+    username,
+    email,
+    password: hashPassword
+})
+    const newUser=await user.save()
+    console.log(newUser)
+    //sendEmail(email,"Welcome to our app","Welcome to our app")
+    await sendEmail({email,message:"Welcome to our app"})
+}catch (e) {}
+
+}
